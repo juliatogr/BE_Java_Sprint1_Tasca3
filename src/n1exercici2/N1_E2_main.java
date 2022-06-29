652c03ba9d1f117c0e4cd2dba588d8575dd3b8c0
juliatogr <julia.tortosa01@estudiant.upf.edu>
2022-06-29 10:01:51 +0200

    n1e2 final

--- src/n1exercici2/N1_E2_main.java
+++ src/n1exercici2/N1_E2_main.java
@@ -1,10 +1,26 @@
 package n1exercici2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ListIterator;
+
 public class N1_E2_main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<Integer> ints1 = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+		System.out.println("Primera llista d'enters: " + ints1);
+		
+		List<Integer> ints2 = new ArrayList<Integer>();
+		System.out.println("Creem una segona llista d'enters buida: " + ints2);
+		
+		System.out.println("Agegint els enters de la primera llista en ordre invers a la segona llista... ");
+		ListIterator<Integer> intsIt = ints1.listIterator(ints1.size());
+	
+		while (intsIt.hasPrevious()) {
+			ints2.add(intsIt.previous());
+		}
+		System.out.println("Segona llista d'enters: " + ints1);
 	}
 
 }
